ea9c86013b661ad7c0e4fcade3ca98769e454bfc
t-induwara <tharusha.induwar@gmail.com>
2023-08-18 14:20:40 +0530

    Added variables halfway

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 console.log("Script loaded");
 
+//Tab number section variables
+var tab1 = document.getElementById("num-1");
+var tab2 = document.getElementById("num-2");
+var tab3 = document.getElementById("num-3");
+var tab4 = document.getElementById("num-4");
+
+tab2.classList.add("active-num-col");
+
 //Form container variables
+var frmName = document.getElementById("frm-name").value;
+var frmEmail = document.getElementById("frm-mail").value;
+var frmPhone = document.getElementById("frm-phone").value;
+var frmNextBtn = document.getElementById("frm-next-btn");
 
 //Plan container variables
+//Plan1
+var plan1 = document.getElementById("plan-1");
+//Plan2
+var plan2 = document.getElementById("plan-2");
+//Plan2
+var plan3 = document.getElementById("plan-3");
+
+
+
+var planBackBtn = document.getElementById("plan-back-btn");
+var planNextBtn = document.getElementById("plan-next-btn");
 
 //Addons container variables
 
